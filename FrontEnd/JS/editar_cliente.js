8649ecc8a2600c53bc1a7aeaf3f332e929f9ab1f
Guilherme Santos <guilhermehero20@gmail.com>
2024-04-30 12:56:57 -0300

    editar

--- FrontEnd/JS/editar_cliente.js
+++ FrontEnd/JS/editar_cliente.js
@@ -1,7 +1,21 @@
-const meuInput = document.getElementById('cliente_id');
+async function carregarDadosCliente() {
+    try {
+        const clienteId = document.getElementById('cliente_id').value;
+        const url = `/perfilcliente/${clienteId}`;
 
-meuInput.addEventListener('blur', function(event) {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Erro ao obter dados do cliente');
+        }
 
-    const valorInput = event.target.value;
-    //console.log('Número inserido:', valorInput);
-});
+        const dados = await response.json();
+
+        // Preencher os campos de entrada com os dados obtidos
+        document.getElementById('name').value = dados.name;
+        document.getElementById('email').value = dados.email;
+        document.getElementById('phone').value = dados.telefone;
+        document.getElementById('password').value = dados.password;
+    } catch (error) {
+        console.error('Erro ao preencher campos do cliente: ', error);
+    }
+}
