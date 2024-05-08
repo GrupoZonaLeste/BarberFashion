04227832cfd7a2e7de88908e4dd8257954aa29cc
GustavoAnjos99 <gustavoanjos160@gmail.com>
2024-05-08 20:02:09 -0300

    Create/read funcionario corrigido

--- FrontEnd/JS/cliente/modal.js
+++ FrontEnd/JS/cliente/modal.js
@@ -1,5 +1,6 @@
 const modal = document.querySelector('.modal')
 const modal2 = document.querySelector('.modal2')
+const modal3 = document.querySelector('.modal3')
 
 
 const switchModal = () =>{
@@ -8,17 +9,27 @@ const switchModal = () =>{
 const switchModal2 = () =>{
     modal2.style.display = 'block'
 }
+const switchModal3 = () =>{
+    modal3.style.display = 'block'
+}
 
 
 const btn = document.querySelector('.modalBtn')
 btn.addEventListener('click', switchModal)  
 const btn2 = document.getElementById('btn-editar-servicos')
+const btn2_1 = document.getElementById('btn-editar-servicos')
 btn2.addEventListener('click', switchModal2)
+btn2_1.addEventListener('click', switchModal3)
 
 const btn3 = document.getElementById('fechar_agendamento_btn')
 btn3.addEventListener('click', ()=> {
     modal.style.display = 'none'
 })
+const btn3teste = document.getElementById('fechar_agendamento_btn2')
+btn3teste.addEventListener('click', ()=> {
+    modal3.style.display = 'none'
+})
+
 
 const btn4 = document.getElementById('fechar_servicos_btn')
 btn4.addEventListener('click', () => {
