e6040adb6074c5ec8d6aa6ff4aa69ee6b0531733
Diego Bicelli <72448015+DiegoBicelli@users.noreply.github.com>
2024-05-14 21:37:42 -0300

    Implemented (editar_foto)

--- BackEnd/router/cadastro.py
+++ BackEnd/router/cadastro.py
@@ -9,6 +9,11 @@ from urllib.parse import unquote
 from controllers.tokens import Token
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from typing import Dict
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import shutil
+import os
+from fastapi.staticfiles import StaticFiles
 
 db_handle = DBConnectionHandler()
 db_handle.connect_to_db()
@@ -26,6 +31,27 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.mount("/static", StaticFiles(directory="clientes_pictures"), name="static")
+@app.post("/upload/")
+async def upload_image(image: UploadFile = File(...), id: int = Optional):
+    # Diretório onde as imagens serão salvas
+    upload_folder = "clientes_pictures"
+    file_extension = os.path.splitext(image.filename)[1]
+
+    # Cria o novo nome do arquivo usando o ID do usuário e a extensão do arquivo original
+    new_filename = f"user_{id}{file_extension}"
+
+    # Caminho completo para salvar a imagem
+    image_path = os.path.join(upload_folder, new_filename)
+   
+    # Cria o diretório de upload se não existir
+    os.makedirs(upload_folder, exist_ok=True)
+
+    # Salva a imagem no diretório
+    with open(image_path, "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return JSONResponse({"filename": new_filename})
 
 @app.post("/cadastrar/")
 async def add_item(cliente: Cliente):
