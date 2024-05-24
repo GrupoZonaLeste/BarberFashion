1e24dedd46fb04ad73c252d0424d1656d38a2600
Guilherme Santos <guilhermehero20@gmail.com>
2024-05-23 22:42:09 -0300

    Aba Serviços pronta
    Falta função no js para mudar os estados dos modais e a navegação

---
+++ FrontEnd/JS/cliente/opcoes_servicos.js
@@ -0,0 +1,38 @@
+const btnservicos = document.getElementById('btnservicos')
+const btnhorarios = document.getElementById('btnhorarios')
+const btnbarbeiros = document.getElementById('btnbarbeiros')
+const btnconfirmacao = document.getElementById('btnconfirmacao')
+
+const cortes = document.getElementById('cortes')
+const barbas = document.getElementById('barbas')
+barbas.style.display = 'none';
+const sobrancelha = document.getElementById('sobrancelha')
+sobrancelha.style.display = 'none';
+const tratamentos = document.getElementById('tratamentos')
+tratamentos.style.display = 'none';
+
+btnservicos.addEventListener('click', () =>{
+    dis
+})
+
+btnservicos.onmousemove = function () {
+    btnservicos.style.opacity = 0.5
+}
+btnservicos.onmouseout = function () {
+    btnservicos.style.opacity = 1
+}
+btnservicos.addEventListener('click', () =>{
+
+})
+
+btnhorarios.addEventListener('click', () =>{
+
+})
+
+btnbarbeiros.addEventListener('click', () =>{
+
+})
+
+btnFechar_relat.addEventListener('click', () => {
+   
+})
