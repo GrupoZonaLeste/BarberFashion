88088e1eabf067a9655bda7d9232b5e656f8d9aa
GustavoAnjos99 <132578544+GustavoAnjos99@users.noreply.github.com>
2024-06-25 19:06:38 -0300

    Update database.py

--- BackEnd/database/database.py
+++ BackEnd/database/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from bson import ObjectId
 
 
-MONGO_CONNECTION_STRING = MongoClient("mongodb://localhost:27017")
+MONGO_CONNECTION_STRING = MongoClient("mongodb://mongo:IPCrnLmUBIxbhIMrgnBOGTCeNxnavxOp@viaduct.proxy.rlwy.net:39450")
 database = MONGO_CONNECTION_STRING['Barbearia']
 
 
